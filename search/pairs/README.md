5c89bbcbefa888784a5adf31f49334d43c2e3cb1
Jose Cadena <jose.cadena01@gmail.com>
2013-07-20 17:57:22 -0400

    Solved search/pairs

---
+++ search/pairs/README.md
@@ -0,0 +1,28 @@
+<p>Given N integers [N&lt;=10^5], count the total pairs of integers that have a difference of K. [K&gt;0 and K&lt;1e9]. Each of the N integers will be greater than 0 and at least K away from 2^31-1 (Everything can be done with 32 bit integers).</p>
+
+<p><strong>Input Format</strong> </p>
+
+<p>1st line contains N &amp; K (integers).<br>
+2nd line contains N numbers of the set. All the N numbers are assured to be distinct.</p>
+
+<p><strong>Output Format</strong>  </p>
+
+<p>One integer saying the number of pairs of numbers that have a diff K.  </p>
+
+<p><strong>Sample Input #00:</strong></p>
+
+<p>5 2<br>
+1 5 3 4 2  </p>
+
+<p><strong>Sample Output #00:</strong> </p>
+
+<p>3</p>
+
+<p><strong>Sample Input #01:</strong></p>
+
+<p>10 1<br>
+363374326 364147530 61825163 1073065718 1281246024 1399469912 428047635 491595254 879792181 1069262793&nbsp;</p>
+
+<p><strong>Sample Output #01:</strong></p>
+
+<p>0</p>
---
+++ search/pairs/pairs.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+def sol1(n,k,arr):
+    '''
+        First idea: Check if i and i-k are part of arr by creating 
+        an array 'exists' of length max(arr). In this array, the ith entry 
+        indicates if the number i is in arr (True) or not (False).
+        Then, simply multiply the ith and (i-k)th entry in 'exists'.
+        This product will be either 1 (which means that we found a pair 
+        whose difference is k) or 0 (either i or i-k are missing).
+
+        Space complexity: O(max(arr)) =  O(2^31-1)
+        Time complexity: O(n + max(arr))
+
+        Which makes this algorithm pseudo-polynomial and very slow for
+        high values of max(arr)
+    '''
+    exists = [False]
+    for i in xrange(max(arr)):
+        exists.append(i+1 in arr)
+    minuend = exists[k+1:]
+    subtrahend = exists[1:-k]
+    return sum([minuend[i]*subtrahend[i] for i in xrange(len(minuend))])
+
+def sol2(n,k,arr):
+    '''
+        Second idea: Same intuition as before. For each number i in arr, 
+        we want to check if i+k is in arr as well.
+        In this case, create an array 'shifted' of length n. Let x be the 
+        ith entry in arr; then, the ith entry in 'shifted' is x+k.
+        We treat arr and shifted as sets, and find the interception, which
+        implicitely tells us if both x and x+k are in arr.
+
+        Space complexity: O(n)
+        Time complexity: O(n)
+
+        This version passes the hardest test case in 0.22 seconds.
+    '''
+    shifted = [i+k for i in arr]
+    return len(set(arr).intersection(set(shifted)))
+    
+
+def main():
+    #reading the input
+    n,k = [int(i) for i in raw_input().split()]
+    arr = [int(i) for i in raw_input().split()]
+
+    print sol2(n,k,arr)
+
+if __name__=='__main__':
+    main()
